f15490b478d9e63b8a999462a5c0442c30683ffe
Antony Nganga <antonynganga05@gmail.com>
2019-08-28 22:43:33 +0300

    Add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { StudioComponent } from './studio/studio.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    StudioComponent,
+    AboutComponent,
+    ServicesComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
