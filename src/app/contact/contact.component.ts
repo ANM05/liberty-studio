4a4f86248e3e0903dda60fc0542a6c0a379ac1c0
Antony Nganga <antonynganga05@gmail.com>
2019-09-01 23:59:10 +0300

    Add maps service

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  public map: any = { lat: 51.678418, lng: 7.809007 };
 
   constructor() { }
 
