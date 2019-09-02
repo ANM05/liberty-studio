e95cdf9880d9486ae651163e66deb2531b7100b5
Antony Nganga <antonynganga05@gmail.com>
2019-09-02 22:43:22 +0300

    Update project version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyAD-kOlSh5FKsM2uYfbMsmzxBkRi-J0AQQ'
 };
 
 /*
